c9637a3c01f694842c3294ddb3ecae719b443201
xcb3d <xcode3d@gmail.com>
2024-10-21 10:44:28 +0700

    update delay

--- star_edunext.js
+++ star_edunext.js
@@ -30,7 +30,7 @@ if (gradeButton) {
               if (count >= 5) {
                   clearInterval(intervalId);
               }
-          }, 100); // 100ms giữa mỗi lần click
+          }, 400); // 100ms giữa mỗi lần click
       } else {
           console.log("Không tìm thấy button có span chứa 'Grade', đang tìm lại...");
       }
@@ -47,7 +47,7 @@ if (gradeButton) {
           clearInterval(searchInterval);
           findAndClickGradeButton();
       }
-  }, 100); // 100ms giữa mỗi lần tìm
+  }, 400); // 100ms giữa mỗi lần tìm
 } else {
   console.log("Không tìm thấy button có span chứa 'Grade on groupmates'.");
 }
